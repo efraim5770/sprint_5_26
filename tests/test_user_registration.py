b83b0cebfb02bbc93f59d1078da399bb6f460e6b
efraim5770 <95578789+efraim5770@users.noreply.github.com>
2025-07-29 16:47:00 +0300

    Update test_user_registration.py

--- tests/test_user_registration.py
+++ tests/test_user_registration.py
@@ -1,18 +1,17 @@
-import pytest
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from src.helpers import (
-    generate_name, generate_login, generate_valid_password,
-    generate_invalid_password
-)
+
 from src.locators import WebsiteLocators as L
-import src.data as data
+from src.helpers import (
+    generate_name, generate_login, generate_valid_password, generate_invalid_password
+)
+from src.data import Urls
 
 
 class TestUserRegistration:
 
     def test_successful_registration(self, driver):
-        driver.get(data.register_page_url)
+        driver.get(Urls.register())
         wait = WebDriverWait(driver, 10)
 
         wait.until(EC.presence_of_element_located(L.NAME_INPUT_FORM)).send_keys(generate_name())
@@ -21,11 +20,11 @@ class TestUserRegistration:
 
         wait.until(EC.element_to_be_clickable(L.REGISTRATION_BUTTON_FORM)).click()
 
-        wait.until(EC.url_to_be(data.login_page_url))               # доп. валидация
+        wait.until(EC.url_to_be(Urls.login()))               # валидация редиректа
         assert wait.until(EC.visibility_of_element_located(L.LOGIN_BUTTON_FORM)).is_displayed()
 
     def test_registration_with_invalid_password(self, driver):
-        driver.get(data.register_page_url)
+        driver.get(Urls.register())
         wait = WebDriverWait(driver, 10)
 
         wait.until(EC.presence_of_element_located(L.NAME_INPUT_FORM)).send_keys(generate_name())
